fd133cdc492283a5b713454377eb0d4c38991a65
Hrishikesh kumbhar <hrishikumbhar156@gmail.com>
2021-11-09 21:51:53 +0530

    added dark mode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import './App.css';
 import React, {Component} from 'react';
 import Particles from 'react-particles-js';
+import Toggle from './components/toggle/Toggle';
 import Navbar  from './components/Navbar/Navbar';
 import Card  from './components/Card/Card';
 import Tech from './components/Tech/Tech';
 import Contact  from './components/Contact/Contact';
 import Hand from './media/Hand.gif';
-import Dp from './media/dp3.jpeg';
+import Dp from './media/me2.jpg';
 import Tilt from 'react-tilt';
 import Education  from './components/Education/Education';
+import About from './components/About/About';
 const particlesOpt={
         
     
 
   particles: {
       number:{
-          value:200,
+          value:100,
           density:{
               enable:true,
-              value_area:1000
+              value_area:2000
           }
 
       }
@@ -36,14 +38,18 @@ render(){
       params={particlesOpt}
       />
        <Navbar />
+     
 
-      <div className="intro" id="home">
+      <div className="intro" id="home" >
         <span className="wave">
-        Hii &nbsp;
+        <b>Hii</b> &nbsp;
         <img src= {Hand} alt = "hand" width="35px" />
-          &nbsp;, I'm Hrishi.
+        <br/>
+          <b>I'm Hrishi.</b> <br></br>
+          <br></br>
+          
         </span>
-        <div className="center">
+        {/* <span className="center">
         <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
         <div className="Tilt-inner">
         <img src={Dp} alt="me" class="rounded-circle" height="200" width="200" style = {{margin:"40px"}}></img>
@@ -51,23 +57,22 @@ render(){
         </div>
         </Tilt>
 
-        </div>      
-
-        
-        <span className="wave">
-          I'm a Web Developer & Android Developer.
-        </span>
-        <p>
-          
-        </p>
+        </span>       */}
+        </div>
+        <img src={Dp} className="profile" alt="me"  height="600" width="300" style = {{margin:"40px"}}></img>
+      
+        <About />
         <Tech />
         <Card />
         
         <Education />
 
         <Contact />
+        <footer>
+          copyright@ 2021 portfolio website
+        </footer>
 
-      </div>
+      
       
 
 
--- src/components/Tech/Tech.js
+++ src/components/Tech/Tech.js
@@ -2,7 +2,7 @@ import './Tech.css'
 const Tech = () =>{
     return(
         <div className="tech">
-            <h1>Technologies I'm Famlilar with</h1>
+            <h1><b>Technologies I'm Famililar with</b></h1>
             <div className="box">
             <div className="icon">
             <img src="https://img.icons8.com/color/48/000000/java-coffee-cup-logo--v1.png" alt="java"/>
@@ -24,7 +24,7 @@ const Tech = () =>{
             </div>
 
             <div className="icon">
-            <img src="https://img.icons8.com/ios/50/000000/php-logo.png" alt="php"/>
+            <img src="https://img.icons8.com/color/48/000000/php.png" alt="php"/>
             <h3>PHP</h3>
             </div>
 
@@ -34,12 +34,14 @@ const Tech = () =>{
             </div>
 
             <div className="icon">
-            <img src="https://img.icons8.com/ios-filled/50/000000/github.png" alt="github"/>
+            <img src="https://img.icons8.com/nolan/48/github.png" alt="github"/>
+          
             <h3>GITHUB</h3>
             </div>
 
             <div className="icon">
-            <img src="https://img.icons8.com/windows/48/000000/node-js.png" alt="nodejs"/>
+            <img src="https://img.icons8.com/color/48/000000/nodejs.png" alt="nodejs"/>
+      
             <h3>NODE</h3>
             </div>
 
@@ -49,7 +51,8 @@ const Tech = () =>{
             </div>
 
             <div className="icon">
-            <img src="https://img.icons8.com/ios/48/000000/mysql-logo.png" alt="mysql"/>
+            <img src="https://img.icons8.com/nolan/48/mysql.png" alt="mysql"/>
+          
             <h3>MYSQL</h3>
             </div>
 
@@ -59,7 +62,8 @@ const Tech = () =>{
             </div>
 
             <div className="icon">
-            <img src="https://img.icons8.com/ios-filled/48/000000/android-os.png" alt="android"/>
+            <img src="https://img.icons8.com/color/48/000000/android-os.png" alt="android"/>
+        
             <h3>ANDROID</h3>
             </div>
 
@@ -69,7 +73,7 @@ const Tech = () =>{
             </div>
 
             <div className="icon">
-            <img src="https://img.icons8.com/ios-filled/48/000000/typescript.png" alt="typescript"/>
+            <img src="https://img.icons8.com/color/48/000000/typescript.png" alt="typescript"/>
             <h3>TYPERSCRIPT</h3>
             </div>
 
